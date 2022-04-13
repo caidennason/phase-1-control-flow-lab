b213e7d8e272c519061d85f4b3d49f0063df9e45
caiden nason <caidenbnason@gmail.com>
2022-04-13 17:56:49 -0400

    commit tree 00501bff603988e5088f8b46e04dc023475e9f2b

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let message;
+  if (someValue <= 400){
+    message = ('This one is on me!')
+  }  
+  else if (someValue > 400 && someValue < 2000){
+    message = ('That will be twenty bucks.')
+  }
+  else if (someValue > 2500){
+    message = ('No can do.')
+  }
+  else if (someValue > 2000){
+    message = ('I will gladly take your thirty bucks.')
+  }
+
+  return message
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.'
+  }
 }
